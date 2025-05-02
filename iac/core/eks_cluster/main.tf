0fb29a4e66bd9e53388c78ef2e9f7ba233a0935f
Theviki20110 <v.lapadula@reply.it>
2025-05-02 11:14:09 +0200

    up

--- iac/core/eks_cluster/main.tf
+++ iac/core/eks_cluster/main.tf
@@ -51,16 +51,6 @@ resource "aws_eks_access_entry" "this" {
   )
 }
 
-output "debug_principal_arn" {
-  value = format(
-    "arn:%s:iam::%s:role/%s",
-    data.aws_partition.this.partition,
-    data.aws_caller_identity.this.account_id,
-    element(local.roles)
-  )
-}
-
-
 resource "aws_eks_access_policy_association" "this" {
   count        = local.roles == [] ? 0 : length(local.roles)
   cluster_name = aws_eks_cluster.this.name
